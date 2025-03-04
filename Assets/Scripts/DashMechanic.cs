3e91d9f419341fb7c94ebdd9cd146ed92fbe6f45
ViperzzKing <viperzzkingg@gmail.com>
2025-03-04 16:31:41 +1100

    added dash varibles to dash script

--- Assets/Scripts/DashMechanic.cs
+++ Assets/Scripts/DashMechanic.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class DashMechanic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Tooltip("How far the dodge will go")]
+    public float dodgePower = 24f;
+
+    public float dashingTime = 0.2f;
+
+    [Tooltip("If the dodge key shall be spammed or not")]
+    public float dodgeCooldown = 1f;
+
+    [Tooltip("When dodging the amount of frame the player is invincible")]
+    public float iFrames;
+
+    private bool canDash = true;
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
