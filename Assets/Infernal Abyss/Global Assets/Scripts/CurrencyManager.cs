ebf0a6121d787eeeda653b599600c22f8a64802a
ViperzzKing <viperzzkingg@gmail.com>
2025-03-29 19:33:09 +1100

    added working coin

---
+++ Assets/Infernal Abyss/Global Assets/Scripts/CurrencyManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public int currentCoins;
+    private int minCoins;
+
+    PlayerMovementStateMachine movement;
+
+    private void Start()
+    {
+        movement = FindFirstObjectByType<PlayerMovementStateMachine>();
+    }
+
+    private void Update()
+    {
+        if (currentCoins <= minCoins)
+        {
+            currentCoins = minCoins;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentCoins += 1;
+        // Destroy(gameObject);
+    }
+
+    void BuyDash()
+    {
+        currentCoins -= 10;
+        movement.dashCollected = true;
+    }
+
+    void GiveCoins()
+    {
+        currentCoins += 5;
+    }
+}
